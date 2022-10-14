efb3de8f015ad885b1986ae596a09be345edbea1
Hamza Carew <hamzacarew@gmail.com>
2022-10-13 21:38:41 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SPA-CONFIG
+
+Responsible for hosting all common config that is leveraged in single page applications.
---
+++ webpack.config.cjs
@@ -0,0 +1,50 @@
+const path = require('path')
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = {
+    mode: 'development',
+    entry: "./src/root.component.tsx",
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        open: true,
+        port: 1337,
+        host: '127.0.0.1',
+        https: true,
+        historyApiFallback: true,
+        hot: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/i,
+                use: ["css-loader"],
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: path.resolve(__dirname, 'index.html'),
+            filename: 'index.html'
+        }),
+        // new BundleAnalyzerPlugin()
+
+    ]
+};
